186646ec01c34fa91a4d83bb0d7c2e8df1202f54
caioe <caioeike14@gmail.com>
2025-04-25 11:44:37 -0300

    feat: adiciona transformação de JSON para CSV e refatora organização das pastas

--- ETL.py
+++ ETL.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import pandas as pd
 
 # Extract
 # Extrair os dados da API DummyJson
@@ -13,32 +14,48 @@ def extract_data(endpoint):
     return None
 
 # Load
-def load_data(data, path):
+def load_data(data, resource):
+  path = os.path.join("raw", resource)
   os.makedirs(path, exist_ok=True)
 
   id = data["id"]
-  with open(f"{path}/{id}.json", "w", encoding='utf-8') as file:
+  with open(os.path.join(path, f"{id}.json"), "w", encoding='utf-8') as file:
     json.dump(data, file)
 
 # Função para carregar os dados nas respectivas pastas
 def loop_load_data(resource):
   base_url = (f"https://dummyjson.com/{resource}/")
   i = 1
-  while True and i <= 10: # Traz apenas os 10 primeiros
+  while True:
     data = extract_data(base_url + str(i))
-    if data:
+    if data and i <= 10: # Traz os 10 primeiros
       load_data(data, resource)
       i += 1
+    elif i > 10:
+      break
     else:
       print(f"Erro ao extrair os dados da API: {data}")
       break
+
+# Transform
+def transform_data_json_to_csv(endpoint, i):
+  with open(f"raw/{endpoint}/{i}.json", "r", encoding='utf-8') as file:
+    data = json.load(file)
+  
+  path = os.path.join("transformed", endpoint)
+  os.makedirs(path, exist_ok=True)
+
+  df = pd.DataFrame([data])
+  df.to_csv(os.path.join(path, f"{i}.csv"), index = False)
 
 
 endpoints = ["users", "products"]
 
 def main():
   for endpoint in endpoints:
-    loop_load_data(endpoint) 
+  #   loop_load_data(endpoint) 
+    for i in range(1, 11):
+      transform_data_json_to_csv(endpoint, i)
 
 if __name__ == '__main__':
     main()
